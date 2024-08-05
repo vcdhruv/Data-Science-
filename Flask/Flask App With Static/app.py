cea0b010ce40805fdbae9222c791b16e120c29ef
vcdhruv <95232452+vcdhruv@users.noreply.github.com>
2024-08-05 13:41:09 +0530

    flask 2 assignment completed

--- Flask/Flask App With Static/app.py
+++ Flask/Flask App With Static/app.py
@@ -1,12 +1,29 @@
-from flask import Flask,request ,render_template , jsonify
+from flask import Flask,request ,render_template , jsonify , redirect , url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def home_page():
-    return render_template('index.html')
+# @app.route('/')
+# def home_page():
+#     return render_template('index.html')
 
+@app.route("/welcome")
+def welcome():
+    return "<h1>Welcome To ABC Corporation</h1>"
+
+data = {
+        "Company Name" : "ABC Corporation",
+        "Location" : "India",
+        "Contact Detail" : "999-999-9999"
+    }
+
+@app.route('/')
+def detail():
+    return render_template('data.html',data=data)
+
+@app.route("/redirect_to_welcome")
+def redirect_go():
+    return redirect(url_for('welcome'))
 
 @app.route('/math',methods=['POST'])
 def math_ops():
@@ -52,6 +69,7 @@ def math_ops1():
             result = "The divide of " + str(num1) + 'and ' + str(num2) + "is " + str(r)
             
         return jsonify(result)
+    
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",debug=True)
